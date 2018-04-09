acfab09a6931b16f8d6eefa4acdf306982b4dee9
wuzhuyu5 <wuzhiyu@jd.com>
2018-04-09 17:17:40 +0800

    fix travis config

--- karma.conf.js
+++ karma.conf.js
@@ -60,8 +60,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: [!process.env.TRAVIS ? 'Chrome' : 'ChromeNoSandbox'],
 
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
